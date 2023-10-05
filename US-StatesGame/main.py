23e63044b72712fea226fd1882428ed07048a4ac
Miguel <miguelangel.mafer@gmail.com>
2023-10-05 13:24:06 +0200

    Squirrel Census, States Game

---
+++ US-StatesGame/main.py
@@ -0,0 +1,40 @@
+import turtle
+from turtle import Screen
+import pandas
+
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+
+states = data["state"].to_list()
+correct_answer = []
+
+while len(correct_answer) < 50:
+    answer_state = screen.textinput(title=f"Guess the State {len(correct_answer)} / 50",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in correct_answer:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in states:
+        if answer_state not in correct_answer:
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.penup()
+            t.penup()
+            state_data = data[data.state == answer_state]
+            t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+            t.write(answer_state)
+            correct_answer.append(answer_state)
+
+screen.exitonclick()
