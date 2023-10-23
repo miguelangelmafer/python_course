92ee2807a51821ad232b3ec6f1fd7db46c3a264f
Miguel <miguelangel.mafer@gmail.com>
2023-10-23 15:20:05 +0200

    Handling Exception NATO

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -6,6 +6,15 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 word = input("Enter a word: ").upper()
 
-output = [data_dict[letter] for letter in word]
 
-print(output)
+def generate_phonetic():
+    try:
+        output = [data_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
