443fef03ab885e78d8caad21eb5ca85ec5c1aa12
Miguel <miguelangel.mafer@gmail.com>
2023-10-30 11:54:41 +0100

    Automated birthday mail

---
+++ Automated Birthday Wisher/main.py
@@ -0,0 +1,25 @@
+import smtplib
+from datetime import datetime
+import pandas
+import random
+import const
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=const.MY_EMAIL, password=const.MY_PASSWORD)
+        connection.sendmail(from_addr=const.MY_EMAIL, to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{contents}")
