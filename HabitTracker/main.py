b1f3ad4aecd82953387592ed9c0266980d4aa3d7
Miguel <miguelangel.mafer@gmail.com>
2023-11-05 10:55:00 +0100

    Habit Tracker with Pixela

---
+++ HabitTracker/main.py
@@ -0,0 +1,65 @@
+import os
+
+import requests
+from datetime import datetime
+
+USERNAME = os.environ["USERNAME"]
+TOKEN = os.environ["TOKEN"]
+GRAPH_ID = "graph0"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# CREATE A USER
+# response = requests.post(pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Steps Graph",
+    "unit": "Steps",
+    "type": "int",
+    "color": "kuro"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# CREATE A GRAPH
+# response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many steps did you do today?\n")
+}
+
+response = requests.post(pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "5000"
+}
+
+# response = requests.put(update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
