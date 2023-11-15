44ea5163eaa36bc354642b8143fe300c20dc43a3
Miguel <miguelangel.mafer@gmail.com>
2023-11-15 05:10:14 +0100

    Add check null

--- CheapFlightFinder/main.py
+++ CheapFlightFinder/main.py
@@ -41,7 +41,7 @@ for destination in sheet_data:
         to_time=six_month_from_today
     )
 
-    if flight.price < int(destination["lowestPrice"]):
+    if flight.price is not None and flight.price < int(destination["lowestPrice"]):
         telegram_bot_send_text(
             f"Low price alert! Only {flight.price}€ to fly from {flight.origin_city}-{flight.origin_airport} to "
             f"{flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}.")
