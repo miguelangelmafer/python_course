3b0cffec59100656f82819dfa2eb630ff37f0bb5
Miguel <miguelangel.mafer@gmail.com>
2023-12-20 13:41:43 +0100

    Pending Library Style

--- library-project/main.py
+++ library-project/main.py
@@ -1,40 +1,80 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+# Create the extension
+db = SQLAlchemy()
+# initialise the app with the extension
+db.init_app(app)
 
-all_books = []
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            pages=request.form["pages"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    ##READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    all_books = list(result.scalars())
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add", methods=['POST', 'GET'])
-def add():
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        # UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
         return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
 
-    return render_template('add.html')
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
